f31ce5d7d168d46fda559b66feafb2847d772b93
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-02-19 17:14:12 +0900

    22/02/18 \n 로또 번호 화면 꾸미기

--- app/src/main/java/com/kkuber/myapp_02_lotto/MainActivity.kt
+++ app/src/main/java/com/kkuber/myapp_02_lotto/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kkuber.myapp_02_lotto
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,7 @@ import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import org.w3c.dom.Text
 
@@ -51,6 +53,18 @@ class MainActivity : AppCompatActivity() {
 
         initRunButton()
         initAddButton()
+        initClearButton()
+    }
+
+    private fun initClearButton() {
+        clearButton.setOnClickListener {
+            pickNumberSet.clear()
+            numberTextViewList.forEach {
+                it.isVisible = false
+            }
+
+            didRun = false
+        }
     }
 
     private fun initAddButton() {
@@ -74,13 +88,37 @@ class MainActivity : AppCompatActivity() {
             textView.isVisible = true
             textView.text = numberPicker.value.toString()
 
+            setNumberBackground(numberPicker.value, textView)
+
             pickNumberSet.add(numberPicker.value)
+        }
+    }
+
+    private fun setNumberBackground(number : Int, textView : TextView) {
+        when(number) {
+            in 1..10 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_yellow)
+            in 11..20 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_blue)
+            in 21..30 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_red)
+            in 31..40 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_green)
+            else -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_grey)
         }
     }
 
     private fun initRunButton() {
         runButton.setOnClickListener {
             val list = getRandomNumber()
+
+            didRun = true;
+
+            list.forEachIndexed { index, i ->
+                val textView = numberTextViewList[index]
+
+                textView.text = i.toString()
+                textView.isVisible = true
+
+                setNumberBackground(i, textView)
+            }
+
 
             Log.d("MainActivity", "initRunButton : ${list.toString()}");
         }
@@ -96,12 +134,17 @@ class MainActivity : AppCompatActivity() {
 
         val numberList = mutableListOf<Int>().apply {
             for (i in 1..45) {
+                if (pickNumberSet.contains(i)) {
+                    continue
+                }
+
                 this.add(i)
             }
         }
         numberList.shuffle()
 
-        val newList = numberList.subList(0, 6)
+        val newList = pickNumberSet.toList() + numberList.subList(0, (6 - pickNumberSet.size))
+        // 이미 선택한 번호 리스트 + 새롭게 자동 생성할 번호 리스트
 
         return newList.sorted()
     }
